53adc8f5bdf6dcb0334125ab174f6375c45ceaf3
94edwin89 <edwin.judefrancis@gmail.com>
2025-01-02 17:52:39 +0530

    first page of landing page and minor correction in Header

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,41 +1,46 @@
-import React from 'react';
-import {Link} from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 import { socialMediaLinks } from "../../constants/constants.js";
+
 
 
 const Footer = () => {
   return (
     <div className="dark-bg h-auto py-5">
-    <div className='container-1200 mx-auto flex flex-col gap-3'>
-
-      {/* Logo and social Media Links */}
-      <div className="flex justify-between items-center">
-        {/* image */}
-        <img
-          src="https://codelynks.com/wp-content/uploads/2024/07/logo.svg"
-          alt="logo"
-          className="w-[150px] h-[55px]"
-        />
-        {/* follow us */}
-        <div className="flex items-center gap-8">
-          <p className="uppercase light-text font-semibold hover:text-white">Follow Us : </p>
-          <div className="flex items-center gap-5">
-            {
-              socialMediaLinks?.map((link) => (
-                <a href={link.url} target="_blank" className="light-bg rounded-full p-2 group hover:bg-primary transition-colors duration-200">
-                  <link.icon size={16} className="dark-text group-hover:text-white"/>
+      <div className="container-1200 mx-auto flex flex-col gap-3">
+        {/* Logo and social Media Links */}
+        <div className="flex justify-between items-center">
+          {/* image */}
+          <img
+            src="https://codelynks.com/wp-content/uploads/2024/07/logo.svg"
+            alt="logo"
+            className="w-[150px] h-[55px]"
+          />
+          {/* follow us */}
+          <div className="flex items-center gap-8">
+            <p className="uppercase light-text font-semibold hover:text-white">
+              Follow Us :{" "}
+            </p>
+            <div className="flex items-center gap-5">
+              {socialMediaLinks?.map((link) => (
+                <a
+                  href={link.url}
+                  target="_blank"
+                  className="light-bg rounded-full p-2 group hover:bg-primary transition-colors duration-200"
+                >
+                  <link.icon
+                    size={16}
+                    className="dark-text group-hover:text-white"
+                  />
                 </a>
-              ))
-            }
+              ))}
+            </div>
           </div>
         </div>
-
+        <hr className="light-text border-t" />
       </div>
-        <hr className="light-text border-t"/>
-
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
